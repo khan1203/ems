7ab2bf4af8d9df54531cfbad3eba3305e5530934
khan1203 <rk.rifat1203@gmail.com>
2025-04-09 21:46:16 +0600

    alignment updated

--- myenv/ems.py
+++ myenv/ems.py
@@ -16,25 +16,25 @@ leftFrame = CTkFrame(window)
 leftFrame.grid(row=1, column=0)
 
 idLabel= CTkLabel(leftFrame, text="Id: ", font=('arial', 18, 'bold'))
-idLabel.grid(row=0, column=0, padx=10)
+idLabel.grid(row=0, column=0, padx=10, sticky='w')
 
 idEntry = CTkEntry(leftFrame, font=('arial', 15, 'bold'), width=180)
 idEntry.grid(row=0, column=1, pady=10)
 
 nameLabel= CTkLabel(leftFrame, text="Name: ", font=('arial', 18, 'bold'))
-nameLabel.grid(row=1, column=0, padx=10)
+nameLabel.grid(row=1, column=0, padx=10, sticky='w')
 
 nameEntry = CTkEntry(leftFrame, font=('arial', 15, 'bold'), width=180)
 nameEntry.grid(row=1, column=1, pady=10)
 
 PasswordLabel= CTkLabel(leftFrame, text="Password: ", font=('arial', 18, 'bold'))
-PasswordLabel.grid(row=2, column=0,padx=10)
+PasswordLabel.grid(row=2, column=0,padx=10, sticky='w')
 
 passEntry = CTkEntry(leftFrame, font=('arial', 15, 'bold'), width=180)
 passEntry.grid(row=2, column=1, pady=10)
 
 salaryLabel= CTkLabel(leftFrame, text="Salary: ", font=('arial', 18, 'bold'))
-salaryLabel.grid(row=3, column=0,padx=10)
+salaryLabel.grid(row=3, column=0,padx=10, sticky='w')
 
 salaryEntry = CTkEntry(leftFrame, font=('arial', 15, 'bold'), width=180)
 salaryEntry.grid(row=3, column=1, pady=10)
